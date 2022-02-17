84e0e91eb3d7d05b7122fb22fcae61aedb67d08d
Martin Pellicer <martinpelli@outlook.com>
2022-02-17 20:28:21 -0300

    Created Organism class to handle data

---
+++ Organism.cs
@@ -0,0 +1,22 @@
+﻿namespace APIOrganisms
+{
+    internal class Organism
+    {
+
+        private string id;
+        private string name;
+        private string parent;
+
+
+        public Organism(string id, string name, string parent)
+        {
+            this.id = id;
+            this.name = name;
+            this.parent = parent;
+        }
+
+        public string Id { get => id; }
+        public string Name { get => name; }
+        public string Parent { get => parent;  }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -14,10 +17,23 @@ namespace APIOrganisms
             
             string defontanaAPIurl = "https://test.defontana.com/";
             var response = await http.GetStringAsync(defontanaAPIurl);
-            var data = JsonConvert.DeserializeObject(response);
-            Console.WriteLine(data);
-        
-        
+            var data = JObject.Parse(response)["data"];
+            List<Organism> organisms = new List<Organism>();
+
+            foreach(var item in data)
+            {
+                Organism newOrganism = new Organism(item["ID"].ToString(), item["Name"].ToString(), item["Parent"].ToString());
+                organisms.Add(newOrganism);
+                Console.WriteLine(newOrganism.Id);
+                Console.WriteLine(newOrganism.Name);
+                Console.WriteLine(newOrganism.Parent);
+
+            }
+
+
+
+
+
         }
     }
 }
