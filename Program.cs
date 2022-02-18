e9e76b413c689a291da40306480754bf4f0b2682
Martin Pellicer <martinpelli@outlook.com>
2022-02-17 23:02:17 -0300

    Tree created

--- Program.cs
+++ Program.cs
@@ -18,17 +18,16 @@ namespace APIOrganisms
             string defontanaAPIurl = "https://test.defontana.com/";
             var response = await http.GetStringAsync(defontanaAPIurl);
             var data = JObject.Parse(response)["data"];
-            List<Organism> organisms = new List<Organism>();
+            TreeDataStructure organisms = new TreeDataStructure();
 
             foreach(var item in data)
             {
                 Organism newOrganism = new Organism(item["ID"].ToString(), item["Name"].ToString(), item["Parent"].ToString());
-                organisms.Add(newOrganism);
-                Console.WriteLine(newOrganism.Id);
-                Console.WriteLine(newOrganism.Name);
-                Console.WriteLine(newOrganism.Parent);
-
+                organisms.AddItemToTree(newOrganism);
             }
+
+            Organism root = new Organism("0", "root", "");
+            organisms.PrintTree(root, "", true);
 
 
 
---
+++ TreeDataStructure.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace APIOrganisms
+{
+    internal class TreeDataStructure
+    {
+        List<Organism> tree = new List<Organism>();
+
+        public void AddItemToTree(Organism organism)
+        {
+            tree.Add(organism);
+        }
+
+
+        public void PrintTree(Organism organism, String indent, bool last)
+        {
+            Console.WriteLine(indent + "+- " + organism.Name);
+            indent += last ? "   " : "|  ";
+            List<Organism> children = new List<Organism>();
+            children = tree.Where(org => org.Parent == organism.Id).ToList();
+            for (int i = 0; i < children.Count; i++)
+            {
+                PrintTree(children[i], indent, i == children.Count - 1);
+            }
+        }
+
+    }
+}
